41da2be6b23f13e54bd141d1a008dba11053c8db
grayayer <grayayer@gmail.com>
2014-12-05 12:17:29 -0800

    full working switches, with flash

--- js/app.js
+++ js/app.js
@@ -130,46 +130,45 @@ $('#start-jr').on('click', function() {
 
 /**** PERSONAL VERSION ***/
 $(document).ready(function(){
-    // Append the stylesheet on page load
+    // Append the stylesheet on page load 
     if ($.cookie('personalStyle') === "true") {
 	  $('head').append('<link rel="stylesheet" href="/wp-content/themes/attentivepress/css/personal.css" type="text/css" id="personal_stylesheet"/>'); 
-
+	  $("#corporate_stylesheet").remove();
     }
-    // Add the click handler to switch the stylesheet on and off
+    if ($.cookie('personalStyle') === "true") {
+	  $('head').append('<link rel="stylesheet" href="/wp-content/themes/attentivepress/css/personal.css" type="text/css" id="personal_stylesheet"/>'); 
+	  $("#personal_stylesheet").remove();
+    }
+    
+    // Add the click handler to switch the personal stylesheet
     $("#personal_style_btn a").click(function () {
-        if (!($.cookie('personalStyle') === "true")) {
-            $('head').append('<link rel="stylesheet" href="/wp-content/themes/attentivepress/css/personal.css" type="text/css" id="personal_stylesheet"/>');
-            $.cookie('personalStyle','true',{path:'/'});
+        if ($.cookie('personalStyle') === "true") {
+        	alert( "Personal Stylesheet Cookie is TURNED ON " );
         }       
-        else {
-            // remove the high-contrast style
-            $("#personal_stylesheet").remove();
-            $.cookie('personalStyle','false',{path:'/'});
+        else { //do something if the cookie personal stylesheet cookie is turned off
+            alert( "Personal Stylesheet Cookie is OFF " );
+			$('head').append('<link rel="stylesheet" href="/wp-content/themes/attentivepress/css/personal.css" type="text/css" id="personal_stylesheet"/>');
+            $.cookie('personalStyle','true',{path:'/'}); //clicking on button turns personalStyle cookie to true
+            $.cookie('corporateStyle','false',{path:'/'}); //clicking on personal button turns corporateStyle cookie to false
+            alert( "Personal Stylesheet Cookie is now on " );        
         }
     });
-});
-
-/**** CORPORATE VERSION 
-$(document).ready(function(){
-    // Append the stylesheet on page load
-    if ($.cookie('corporateStyle') === "true") {
-	  $('head').append('<link rel="stylesheet" href="/wp-content/themes/attentivepress/css/corporate.css" type="text/css" id="corporate_stylesheet"/>'); 
-
-    }
-    // Add the click handler to switch the stylesheet on and off
+    // Add the click handler to switch the personal stylesheet
     $("#corporate_style_btn a").click(function () {
-        if (!($.cookie('corporateStyle') === "true")) {
-            $('head').append('<link rel="stylesheet" href="/wp-content/themes/attentivepress/css/corporate.css" type="text/css" id="corporate_stylesheet"/>');
-            $.cookie('corporateStyle','true',{path:'/'});
+        if ($.cookie('personalStyle') === "true") {
+            alert( "Personal Stylesheet Cookie is ON, we'll now turn that off and the corporate cookie on " );
+
+        	$('head').append('<link rel="stylesheet" href="/wp-content/themes/attentivepress/css/corporate.css" type="text/css" id="personal_stylesheet"/>');
+            $.cookie('personalStyle','false',{path:'/'}); //clicking on button turns personalStyle cookie to true
+            $.cookie('corporateStyle','true',{path:'/'}); //clicking on personal button turns corporateStyle cookie to false
+            $("#personal_stylesheet").remove();
         }       
-        else {
-            // remove the high-contrast style
-            $("#corporate_stylesheet").remove();
-            $.cookie('corporateStyle','false',{path:'/'});
+        else { 
+            alert( "corporate Stylesheet Cookie is already ON " );
         }
-    });
+    });    
 });
-***/
+
 
 // /*You can add options like expiration or site-wide validity to the cookie, so if you want the cookie to be valid for a year, add this to the cookie command*/
 // $.cookie('personal_style', 'false', {expires: 365});
