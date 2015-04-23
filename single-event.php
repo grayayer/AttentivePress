b456d12080c465c5cb440908b3981ffe7e793115
grayayer <grayayer@gmail.com>
2015-04-23 11:00:27 -0700

    eliminate the Events Calendar intro text from individual pages

--- single-event.php
+++ single-event.php
@@ -1,9 +1,9 @@
+<!-- This is for the "real events" which one finds under "news & events" page, as created through the Events Manager plugin -->
 <?php
 
 get_header(); ?>
 <div class="row">
 	<div class="small-12 large-12 columns" role="main">
-
 	<?php /* Start loop */ ?>
 	<?php while (have_posts()) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
